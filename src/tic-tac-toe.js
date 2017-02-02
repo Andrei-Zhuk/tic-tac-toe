05c9fc0a683241d41be27e1b01bbfd9592288578
Andrei-Zhuk <zhuk_andrey@tut.by>
2017-02-02 14:14:00 +0300

    tic-tac-toe: constructor, getCurrentPlayer, nextTurn are implemented

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,25 @@
 class TicTacToe {
     constructor() {
-
+        this._firstPlayer = 'x';
+        this._secondPlayer = 'o';
+        this._playGround = [[null, null, null], [null, null, null], [null, null, null]]
+        this._winner = null;
+        this._current = this._firstPlayer;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this._current;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this._playGround[rowIndex][columnIndex] === null) {
+            this._playGround[rowIndex][columnIndex] = this._current;
+            if (this._current === this._firstPlayer) {
+                this._current = this._secondPlayer;
+                } else {
+                this._current = this._firstPlayer
+            }
+        }   
     }
 
     isFinished() {
